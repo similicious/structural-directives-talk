69a82cc2bd7a6e590d5d75872b6d9e5da356cfe3
similicious <mail@similicious.de>
2023-05-12 14:42:47 +0200

    Formatting

--- src/app/directives/user-binding/user-binding-demo.component.ts
+++ src/app/directives/user-binding/user-binding-demo.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-binding-demo',
   template: `<h1 class="is-size-3">UserBindingDirective</h1>
-    <app-demo-table appUserBinding></app-demo-table> `,
+    <app-demo-table appUserBinding></app-demo-table>`,
 })
 export class UserBindingDemoComponent {}
